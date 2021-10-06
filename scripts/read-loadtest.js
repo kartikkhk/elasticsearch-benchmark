c361fff9a6f83e9c6ca9b2fef38092454d3d8707
kartikkhk <kartik.rawat@smallcase.com>
2021-10-06 14:14:43 +0530

    Update: randomize read query keywords

--- scripts/read-loadtest.js
+++ scripts/read-loadtest.js
@@ -1,9 +1,11 @@
 import http from "k6/http";
 import { check } from "k6";
+const keywords = getKeywords();
 
 export default function () {
   const startTimestamp = Date.now();
-  const res = http.get("http://localhost:3000/read?keyword=perspiciatis");
+  const keyword = keywords[Math.floor(Math.random() * keywords.length)];
+  const res = http.get(`http://localhost:3000/read?keyword=${keyword}`);
   check(res, {
     "response code was 200": (res) => res.status == 200,
   });
@@ -13,4 +15,19 @@ export default function () {
       duration: Date.now() - startTimestamp,
     })
   );
+}
+
+function getKeywords() {
+  return [
+    "perspiciatis",
+    "iusto",
+    "dignissimos",
+    "fugiat",
+    "repudiandae",
+    "voluptatem",
+    "autem",
+    "corporis",
+    "accusantium",
+    "totam",
+  ];
 }
