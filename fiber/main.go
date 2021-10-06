ac2ca94ca9d2af6b20e93a10fea0c16f09fe81fa
kartikkhk <kartik.rawat@smallcase.com>
2021-10-06 17:01:21 +0530

    Add: sample write request

--- fiber/main.go
+++ fiber/main.go
@@ -12,8 +12,22 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 	utils.ConstructClient()
+
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).SendString("pong")
+	})
+
+	// description {write} post request
+	app.Post("/write", func(c *fiber.Ctx) error {
+		payload := new(utils.CommentDoc)
+		if err := c.BodyParser(&payload); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"status":     "failure",
+				"error":      "Invalid request body",
+				"statusCode": fiber.StatusBadRequest,
+			})
+		}
+		return c.JSON(payload)
 	})
 	err := app.Listen(port)
 	if err != nil {
--- fiber/utils/utils.go
+++ fiber/utils/utils.go
@@ -8,6 +8,14 @@ import (
 
 var client elasticsearch.Client
 
+type CommentDoc struct {
+	PostId int32  `json:"postId"`
+	Id     int32  `json:"id"`
+	Name   string `name:"name"`
+	Email  string `email:"email"`
+	Body   string `body:"body"`
+}
+
 func ConstructClient() {
 	client, err := elasticsearch.NewDefaultClient()
 	if err != nil {
@@ -22,3 +30,12 @@ func ConstructClient() {
 	defer res.Body.Close()
 	log.Println(res)
 }
+
+// func Insert(index string, _type string, data CommentDoc) {
+// 	dataJSON, err := json.Marshall(data)
+// 	body := string(dataJSON)
+// 	ind, err := esclient.Index().
+// 		Index(index).
+// 		BodyJson(body).
+// 		Do(ctx)
+// }
