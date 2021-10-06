2db1bfe089f15244348f357750d7e4b313edf729
kartikkhk <kartik.rawat@smallcase.com>
2021-10-06 16:29:15 +0530

    Setup: basic fiber project

---
+++ fiber/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+const port = ":3000"
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).SendString("pong")
+	})
+	err := app.Listen(port)
+	if err != nil {
+		panic(err)
+	}
+}
